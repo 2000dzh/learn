df751d340a5f761d0d95bb728f21515e1a2db1bd
Ddzh <DddZzHh@163.com>
2023-09-06 09:10:57 +0800

    9/6 webpack基础

--- 构建工具/webpack/build/webpack.dev.conf.js
+++ 构建工具/webpack/build/webpack.dev.conf.js
@@ -1,1 +1,12 @@
 // 开发环境
+const { merge } = require('webpack-merge')
+const common = require('./webpack.base.conf.js')
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    compress: true,
+    port: 8080,
+  },
+})
--- 构建工具/webpack/build/webpack.prod.conf.js
+++ 构建工具/webpack/build/webpack.prod.conf.js
@@ -1,1 +1,15 @@
 // 生产环境
+const { merge } = require('webpack-merge')
+const common = require('./webpack.base.conf.js')
+// webpack5 推荐使用 css-minimizer-webpack-plugin 压缩css (比 optimize-css-assets-webpack-plugin 更强)
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+// 压缩 js
+const TerserPlugin = require('terser-webpack-plugin')
+// 复制静态资源 CopyWebpackPlugin 
+
+module.exports = merge(common, {
+  mode: 'production',
+  optimization: {
+    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+  },
+})
